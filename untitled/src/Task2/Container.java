88a16f31807d53909d36bc1426c2d481fd1148c8
Alex <Chugun777akk@yandex.ru>
2022-11-17 13:38:04 +0700

    add class container

--- untitled/src/Task2/Container.java
+++ untitled/src/Task2/Container.java
@@ -1,4 +1,28 @@
 package Task2;
 
+import java.util.Scanner;
+
 public class Container {
+    private int[] arr;
+    private int size;
+    private Scanner sc;
+
+    public Container() {
+        System.out.print("Введите вместимость контейнера: ");
+        this.sc = new Scanner(System.in);
+        this.size = sc.nextInt();
+       this.arr = new int[size];
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        Container con1 = new Container();
+    }
 }
