0ad075b6ee0381f786f8b8e5f1154150178309da
Alex <Chugun777akk@yandex.ru>
2022-11-18 17:29:41 +0700

    Create task2

--- untitled/src/Task2/ContainerServiceImpl.java
+++ untitled/src/Task2/ContainerServiceImpl.java
@@ -1,4 +1,33 @@
 package Task2;
 
 public class ContainerServiceImpl {
+    public int [] sum (int [] arr1, int [] arr2)
+    {
+        if (arr1.length != arr2.length)
+            throw new RuntimeException("Длина массивов различна");
+        else {
+            int [] totalArr = new int[arr1.length];
+            for (int i = 0; i < arr1.length; i++) {
+                totalArr[i] = arr1[i] - arr2[i];
+            }
+            return totalArr;
+        }
+    }
+    public void showArr(int []arr )
+    {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ContainerServiceImpl uwer = new ContainerServiceImpl();
+        int arr5[] = {1,2,3,4,5};
+        int arr6[] = {1,2,3,4,5};
+        uwer.showArr(uwer.sum(arr5,arr6));
+
+
+
+    }
 }
