546a9774029280ae835ac73cceb8a6d5ae629bd2
Alex <Chugun777akk@yandex.ru>
2022-11-18 19:31:17 +0700

    add class main in Task3

--- untitled/src/Task3/Connect.java
+++ untitled/src/Task3/Connect.java
@@ -1,4 +1,11 @@
 package Task3;
 
 public class Connect {
+    public void connection()
+    {
+        Task3 task = new Task3();
+        int [] arr1 = task.fillArray();
+        int [] arr2 = task.fillArray();
+        task.quotient(arr1,arr2);
+    }
 }
